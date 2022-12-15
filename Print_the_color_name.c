f33441b2a1c4d1c669b7523b4393b7ae9277cb3c
MRISHISADWIK <120294677+MRISHISADWIK@users.noreply.github.com>
2022-12-15 09:55:27 +0530

    #include<stdio.h> int main() {     char col;     scanf("%c",&col);     if(col=='V')     {         printf("Violet");     }     else if(col=='I'){         printf("Indigo");
        }
        else if(col=='B')
        {
            printf("Blue");
        }
        else if(col=='G') 
        {
            printf("Green");
        }
        else if(col=='Y')
        {
            printf("Yellow");
        }
        else if(col=='O')
        {
            printf("Orange");
        }
        else if(col=='R')
        {
            printf("Red");
        }
        else
        {
            printf("-1");
        }
        
            
        
    }

---
+++ Print_the_color_name.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    char col;
+    scanf("%c",&col);
+    if(col=='V')
+    {
+        printf("Violet");
+    }
+    else if(col=='I'){
+        printf("Indigo");
+        
+    }
+    else if(col=='B')
+    {
+        printf("Blue");
+    }
+    else if(col=='G') 
+    {
+        printf("Green");
+    }
+    else if(col=='Y')
+    {
+        printf("Yellow");
+    }
+    else if(col=='O')
+    {
+        printf("Orange");
+    }
+    else if(col=='R')
+    {
+        printf("Red");
+    }
+    else
+    {
+        printf("-1");
+    }
+    
+        
+    
+}
