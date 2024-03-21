ba0b3f0d046f9d8df0ede9a48a9e1c114e9b8690
MRISHISADWIK <120294677+MRISHISADWIK@users.noreply.github.com>
2024-03-21 19:58:23 +0530

    #include<stdio.h> int main() {     int n,res;     while(true)     {     	 scanf("%d",&n);     	  if(n==-1)    		 {     	    break; 	   	 }     	 res= n*n;          printf("%d ",res);     }
    }

---
+++ Infinite.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,res;
+    while(true) 
+    {
+    	 scanf("%d",&n);
+    	  if(n==-1)
+   		 {
+    	    break;	
+	   	 }
+    	 res= n*n;
+         printf("%d
+",res);
+    }
+   
+    
+}
